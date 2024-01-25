06c382559da81e2b0045800384b6b007eedd51c9
z0l0git <zolbayar.in@gmail.com>
2024-01-25 09:30:54 +0800

    git ignore

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,40 +1,49 @@
-import express from "express";
-import fs from "fs";
-import mock from "./mock.json" assert { type: "json" };
+// Access and Delete a JSON file
 
-const port = 8000;
+// import express from "express";
+// import { unlink, access } from "node:fs/promises";
+
+// const port = 8000;
+// const app = express();
+
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`);
+// });
+
+// const filePath = "/Users/23LP5833/Desktop/backend/nodejs/mock.json";
+
+// app.get("/", async (req, res) => {
+//   try {
+//     await access(filePath);
+//     res.status(200);
+//     res.set("Content-Type", "application/json");
+//     res.sendFile(filePath);
+//   } catch (err) {
+//     res.status(404).send("File not found");
+//   }
+// });
+
+// app.delete("/", async (req, res) => {
+//   try {
+//     res.status(200);
+//     await unlink(filePath);
+//     res.send("success");
+//   } catch (err) {
+//     res.status(404).send("File Deleted Already");
+//   }
+// });
+
+// 2. index.js User Checker
+
+// import mock from "./mockuser.json" assert { type: "json" };
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
 const app = express();
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
-});
-
-app.get("/", (req, res) => {
-  res.status(200);
-  res.setHeader("Content-Type", "application/json");
-  try {
-    res.send(mock);
-  } catch (err) {
-    res.send("file missing");
-  }
-});
-
-app.delete("/", (req, res) => {
-  res.status(200);
-  fs.unlink("./mock.json", (err) => {
-    if (err) {
-      // An error occurred while deleting the file
-      if (err.code === "ENOENT") {
-        // The file does not exist
-        console.error("The file does not exist");
-      } else {
-        // Some other error
-        console.error(err.message);
-      }
-    } else {
-      // The file was deleted successfully
-      console.log("The file was deleted");
-    }
-  });
-  res.end("deleted");
 });
